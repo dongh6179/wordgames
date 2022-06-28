fe63cbcc6e31925f869f1a415eb46ca2e8e23319
dongh6179 <dongh6179@gmail.com>
2022-06-28 11:41:07 -0400

    added function and words array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,20 @@
+var words = ["akali", "dog", "needle", "baseball", "partner", "manager",
+"absent", "digital", "transmission", "welcome", "context", "command",
+"contradiction", "dare", "owl", "preference", "respectable", "snatch",
+"advance", "organ", "direction", "spectrum", "surgeon", "wheel", "generation",
+"maximum", "adult", "art", "pierce", "kit", "tablet", "necklace", "incentive"]
+
+var wins = 0, losses = 0, numGuesses = 0;
+
 document.onkeyup = function (event) {
     var input = event.key;
     
+}
+
+function updateGame(){
+
+}
+
+function resetGame(){
+
 }
